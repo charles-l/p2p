3c7047585325acafab855c5eeaad0df02d150a1a
ncharlie <charles.saternos@gmail.com>
2017-10-16 17:01:38 -0400

    refactoring

--- lookup.erl
+++ lookup.erl
@@ -1,76 +1,61 @@
 -module(lookup).
 -compile([export_all]).
 
-closest_node(NodeID, SuccessorID, Key) ->
-    MaxKey = math:pow(30, 2),
+-define(KEYLEN, 30).
+
+% largest possible node ID is 2^KEYLEN
+-define(MAXKEY, math:pow(2, ?KEYLEN)).
+
+dist(A, B) ->
     if
-        NodeID > SuccessorID, (MaxKey - SuccessorID - Key) > (Key - NodeID) -> NodeID;
-        NodeID > SuccessorID -> SuccessorID;
-        (Key - NodeID) > (SuccessorID - Key) -> SuccessorID;
-        true -> NodeID
+        A == B -> 0;
+        A < B -> B - A;
+        true -> ?MAXKEY + (B - A)
     end.
 
-get_id_pair(N) ->
-    N ! {id, self()},
-    CID = receive I -> I end,
-    N ! {get_successor_id, self()},
-    NID = receive J -> J end,
-    {CID, NID}.
-
-find_node(Start, Key) ->
-    InnerFind = fun (Current, Next) ->
-                        if
-                            Current <= Key < Next -> {Current, CurrentNext};
-                            Key > Current > Next -> {Current, CurrentNext};
-                            true -> InnerFind(Next, NextNext)
-                        end,
-                        {C, CN} = InnerFind(Start, StartNext),
-                        closest_node(C, CN, Key).
-
+-record(node, {serverid, successor_pid, hashtbl}).
 
 servername(Pid) ->
     {_, [{IpTuple,_,_}|_]} = inet:getif(),
     Id = crypto:hash(md4, lists:flatten(io_lib:format("~p~p", [IpTuple, Pid]))),
     lists:concat(lists:map(fun(X) -> integer_to_list(X, 16) end, binary:bin_to_list(Id))).
 
--record(node, {pid, id, suc, tbl}).
+make_node(Succ) ->
+    #node{serverid=servername(self()), successor_pid=Succ, hashtbl=dict:new()}.
 
-serve(Tbl, Suc) ->
+serve(ThisNode) ->
     receive
         {id, P} ->
-            P ! servername(self()),
-            serve(Tbl, Suc);
+            P ! ThisNode#node.serverid,
+            serve(ThisNode);
         {get_successor, P} ->
-            P ! Suc,
-            serve(Tbl, Suc);
+            P ! ThisNode#node.successor_pid,
+            serve(ThisNode);
         {set_successor, S} ->
-            io:format("Changing successor from ~s to ~s~n", [Suc, S]),
-            serve(Tbl, S);
-        {forward, M} -> % forward a message to sucessor
-            Suc ! M,
-            serve(Tbl, Suc);
+            io:format("Changing successor from ~p to ~p~n", [ThisNode#node.successor_pid, S]),
+            serve(ThisNode#node{successor_pid=S});
         {insert, K, V} ->
             io:format("adding ~s -> ~s~n", [K, V]),
-            serve(dict:store(K, V, Tbl), Suc);
+            serve(dict:store(K, V, ThisNode#node.hashtbl));
         {lookup, K, D} ->
-            D ! dict:find(K, Tbl),
+            D ! dict:find(K, ThisNode#node.hashtbl),
             % TODO: send something back
-            serve(Tbl, Suc);
+            serve(ThisNode);
         R ->
             io:format("Got ~s~n", [R]),
-            serve(Tbl, Suc)
+            serve(ThisNode)
     end.
 
 run() ->
-    Pid = spawn(?MODULE, serve, [dict:new(), "0"]),
-    Pid1 = spawn(?MODULE, serve, [dict:new(), servername(Pid)]),
+    Pid = spawn(?MODULE, serve, [make_node(nil)]),
+    Pid1 = spawn(?MODULE, serve, [make_node(Pid)]),
     Pid ! {id, self()},
     receive
         K -> io:format("Id of child ~s~n", [K])
     end,
 
-    Pid1 ! {successor, Pid},
-    Pid ! {successor, Pid1},
+    Pid1 ! {set_successor, Pid},
+    Pid ! {set_successor, Pid1},
     Pid ! request1,
     Pid ! {insert, "HI", "Yo"},
     ok.
