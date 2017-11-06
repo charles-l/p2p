eccc3140b2640d613272274b325200ab4cdd8608
ncharlie <charles.saternos@gmail.com>
2017-11-06 11:35:33 -0500

    more docs

---
+++ dht/README.md
@@ -0,0 +1,25 @@
+# Design
+
+Generate a ring a ring:
+* Spawn a ring of nodes, each which knows the pid of the next node (basically a circular linked list)
+* Generate an hash (referred to as the id) for every node in the server.
+* Sort the ring by hash
+
+To insert a key, value pair:
+* Hash the key
+* Find which two nodes it fits between (based on id).
+* Insert it into the hash table for the node with the lower id
+
+To lookup a key:
+* Hash the key
+* Find which two nodes it fits between (based on id)
+* Look it up in the hash table for the node with the lower id
+
+# Lessons learned
+
+* Distributed applications are hard (especially from a timing perspective)
+* A simple DHT can be implemented as a linked list of buckets
+
+# Use case
+
+A DHT can be used to make a redundent data store that has a simple interface of appearing to be a regular hash table
--- dht/dht.erl
+++ dht/dht.erl
@@ -17,26 +17,26 @@
 -export([insert/3, lookup/2, spawn_ring/1, spawn_node/1, forward/3]).
 -export([init/1, handle_call/3, handle_cast/2, terminate/2]).
 -behavior(gen_server).
--define(TIMEOUT, 3000).
+-define(TIMEOUT, 3000). % global timeout for casts
 
 -record(state, {
-          next = nil,
-          id = nil,
-          tbl = nil
+          next = nil, % next pid
+          id = nil, % the server id
+          tbl = nil % the servers local hashtable
 }).
 
-hashstr(S) -> % hash a string and return a string
+hashstr(S) -> % hash a string and return a string of the hash
     <<H:160/big-unsigned-integer>> = crypto:hash(sha, S),
     lists:flatten(io_lib:format("~40.16.0b", [H])).
 
-genid() -> % convert IP address + pid into a hashed value
+genid() -> % convert IP address + pid into a hashed value (so each server gets a unique id)
     {_, [{IpTuple,_,_}|_]} = inet:getif(),
     hashstr(lists:flatten(io_lib:format("~p~p", [IpTuple, self()]))).
 
-insert(Node, Key, Val) ->
+insert(Node, Key, Val) -> % generate insert call to add a key, value pair to the dht
     gen_server:cast(Node, {insert, hashstr(Key), Val}).
 
-lookup(Node, Key) ->
+lookup(Node, Key) -> % generate a lookup call and wait for response
     K = hashstr(Key),
     gen_server:cast(Node, {lookup, K, self()}),
     Val = receive
@@ -45,25 +45,29 @@ lookup(Node, Key) ->
           end,
     Val.
 
-forward(Node, N, Msg) ->
+forward(Node, N, Msg) -> % generate a forward call to forward message around ring
     gen_server:cast(Node, {forward, N, Msg}).
 
-spawn_ring(N) ->
+spawn_ring(N) -> % spawn an initial ring of N servers (with buckets sorted)
     {ok, Head} = gen_server:start_link(?MODULE, #state{}, []),
     gen_server:call(Head, {set_next, Head}),
     lists:map(
-      fun({_, X}) -> gen_server:call(X, {join_ring, Head}) end,
-      [gen_server:start_link(?MODULE, #state{}, []) || _ <- lists:seq(1, N)]),
+      fun({_, X}) -> gen_server:call(X, {join_ring, Head}) end, % have each spawned server join the ring
+      [gen_server:start_link(?MODULE, #state{}, []) || _ <- lists:seq(1, N)]), % spawn N servers
     Head.
 
-spawn_node(Ring) ->
+spawn_node(Ring) -> % spawn a node and make it join the ring
     {ok, NewNode} = gen_server:start_link(?MODULE, #state{}, []),
     gen_server:call(NewNode, {join_ring, Ring}),
     NewNode.
 
+% init function for gen_server behavior
 init(S) -> {ok, S#state{id=genid(), tbl=dict:new()}}.
+% terminate function for gen_server behavior
 terminate(_, _) -> ok.
 
+% determines if NewID should go between NodeID and NextID
+% (handles edge case of ring wrap around)
 is_between_buckets(NewID, NodeID, NextID) ->
     IsAtTail = NodeID > NextID,
     if
@@ -74,6 +78,10 @@ is_between_buckets(NewID, NodeID, NextID) ->
             false
     end.
 
+% creates a find_bucket_pair call
+% which walks around the Ring and checks whether the ID should be between
+% the current and next node - when found, returns the pair of pids for the current
+% and next node
 find_bucket_pair(Head, ID, PID) ->
     gen_server:cast(Head, {find_bucket_position, ID, PID}),
     receive
@@ -82,15 +90,20 @@ find_bucket_pair(Head, ID, PID) ->
               {error, timeout}
     end.
 
+% get the next nodes id
 get_next_id(State) ->
     gen_server:call(State#state.next, {id}).
 
+% get the id of the server
 handle_call({id}, _From, State) ->
     {reply, State#state.id, State};
+% get the pid of the next node
 handle_call({next}, _From, State) ->
     {reply, State#state.next, State};
+% update the pid for the next node
 handle_call({set_next, N}, _From, State) ->
     {reply, ok, State#state{next = N}};
+% join a ring starting at the head node
 handle_call({join_ring, Head}, _From, State) ->
     case find_bucket_pair(Head, State#state.id, self()) of
         {error, timeout} ->
@@ -100,7 +113,7 @@ handle_call({join_ring, Head}, _From, State) ->
             io:format("~p joined ~p ~p~n", [self(), Node, NextNode]),
             {reply, ok, State#state{next = NextNode}}
     end.
-
+% lookup a key in the dht and return the value to the sender
 handle_cast({lookup, Key, From}, State) ->
     case is_between_buckets(Key, State#state.id, get_next_id(State)) of
         true ->
@@ -110,9 +123,11 @@ handle_cast({lookup, Key, From}, State) ->
             gen_server:cast(State#state.next, {lookup, Key, From})
     end,
     {noreply, State};
+% find the bucket position when there's only one node in the ring
 handle_cast({find_bucket_position, _NewID, From}, State) when self() == State#state.next ->
     From ! {target_pair, self(), State#state.next},
     {noreply, State};
+% find the best position for NewID
 handle_cast({find_bucket_position, NewID, From}, State) ->
     case is_between_buckets(NewID, State#state.id, get_next_id(State)) of
         true ->
@@ -121,6 +136,7 @@ handle_cast({find_bucket_position, NewID, From}, State) ->
             gen_server:cast(State#state.next, {find_bucket_position, NewID, From})
     end,
     {noreply, State};
+% insert a key, value pair in to the dht
 handle_cast({insert, Key, Val}, State) ->
     case is_between_buckets(Key, State#state.id, get_next_id(State)) of
         true ->
@@ -130,13 +146,16 @@ handle_cast({insert, Key, Val}, State) ->
             gen_server:cast(State#state.next, {insert, Key, Val}),
             {noreply, State}
     end;
+% drop a message when N is 0
 handle_cast({forward, N, M}, State) when N < 1 ->
     io:format("~p Dropping ~p~n", [State#state.id, M]),
     {noreply, State};
+% forward a message to the next node (repeat N times)
 handle_cast({forward, N, M}, State) ->
     io:format("~p (~p keys in bucket) Forwarding ~p to ~p~n", [self(), dict:size(State#state.tbl), M, State#state.next]),
     gen_server:cast(State#state.next, {forward, N - 1, M}),
     {noreply, State};
+% print every other message received
 handle_cast(R, State) ->
     io:format("Got ~p~n", [R]),
     {noreply, State}.
